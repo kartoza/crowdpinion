f924daf395679c32c4a63bd2ea05f1654b1dc8ef
ann26 <anita@kartoza.com>
2019-02-14 19:15:32 +0700

    fix url and sending on submit

--- django_project/base/urls.py
+++ django_project/base/urls.py
@@ -8,22 +8,21 @@ from .views.question import QuestionCreateView, ResultsView, AnswerView
 
 urlpatterns = [
     # basic app views
-    url(regex=r'^^$',
-        view=QuestionCreateView.as_view(),
-        name='home'),
-    url(regex=r'^results/(?P<unique_id>[\w-]+)/$',
-        view=ResultsView.as_view(),
-        name='results'),
-
     url(regex=r'^api/get-answers/(?P<unique_id>[\w-]+)/$',
         view=GetAnswers.as_view(),
         name='get-answers'),
     url(regex=r'^api/add-answers/(?P<unique_id>[\w-]+)/$',
         view=AddAnswer.as_view(),
         name='add-answers'),
+    url(regex=r'^results/(?P<unique_id>[\w-]+)/$',
+        view=ResultsView.as_view(),
+        name='results'),
     url(regex=r'^(?P<unique_id>[\w-]+)/$',
         view=AnswerView.as_view(),
         name='answers'),
+    url(regex=r'^$',
+        view=QuestionCreateView.as_view(),
+        name='home'),
 ]
 
 # Prevent cloudflare from showing an ad laden 404 with no context
